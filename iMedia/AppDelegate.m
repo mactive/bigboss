42b3642f4ad7ffdbee051108793e185e0718b586
mactive <mactive@gmail.com>
2013-03-05 16:56:35 +0800

    回来闪退

--- iMedia/AppDelegate.m
+++ iMedia/AppDelegate.m
@@ -335,18 +335,18 @@ static const int ddLogLevel = LOG_LEVEL_OFF;
                     newCompany.status = CompanyStateFollowed;
 
                     DDLogVerbose(@"SYNC Insert company success %@",newCompany.companyID);
-                    MOCSave(moc);
                 }
                 // update
                 else{
                     newCompany.status = CompanyStateFollowed;
                     DDLogVerbose(@"SYNC update company success %@",newCompany.companyID);
-                    MOCSave(moc);
 
                 }
-
+#warning mocsave
+//                MOCSave(moc);
             }];
             
+
             [self.updateCompanyTimer invalidate];
         }else{
             _updateCompanyRetryCount += 1;
@@ -419,11 +419,13 @@ static const int ddLogLevel = LOG_LEVEL_OFF;
 
                     }
                     ////////////////////////////////////////////////////////////////////////////////////
+#warning mocsave
+//                    MOCSave(moc);
+
                 }
        
                 
             }];
-            MOCSave(moc);
 
             [self.getLastMessageTimer invalidate];
         }else{
